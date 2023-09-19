a3f7f6411b4f70a80b0d81178fea8026f947c5d7
ns3pw6 <yann.u7426@gmail.com>
2023-09-19 16:42:54 +0800

    post complete

--- api_server/resources/user.py
+++ api_server/resources/user.py
@@ -1,6 +1,16 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask import jsonify
 import pymysql
+import traceback
+
+# 
+# Preventing SQL injection in Python, only accept these fields
+# 
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('gender')
+parser.add_argument('birth')
+parser.add_argument('note')
 
 class Users(Resource):    
     def db_init(self):
@@ -16,3 +26,28 @@ class Users(Resource):
         users = cursor.fetchall()
         db.close()
         return jsonify({'data' : users})
+    
+    def post(self):
+        db, cursor = self.db_init()
+        arg = parser.parse_args()
+        user = {
+            'name' : arg['name'],
+            'gender' : arg['gender'],
+            'birth' : arg['birth'],
+            'note' : arg['note']
+        }
+        sql = """
+            INSERT INTO `apitest`.`users` (`name`, `gender`, `birth`, `note`) VALUES ('{}', '{}', '{}', '{}');
+        """.format(user['name'], user['gender'], user['birth'], user['note'])
+        
+        response = {}
+        try:
+            cursor.execute(sql)
+            response['msg'] = 'Success'
+        except:
+            traceback.print_exc()
+            response['msg'] = 'Failed'
+            
+        db.commit()
+        db.close()
+        return jsonify(response)
