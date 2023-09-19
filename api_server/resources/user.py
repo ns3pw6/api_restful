da47b238ea4a23a8de99b067a174ccfdba0313c1
ns3pw6 <yann.u7426@gmail.com>
2023-09-19 16:26:46 +0800

    get complete

--- api_server/resources/user.py
+++ api_server/resources/user.py
@@ -1,8 +1,18 @@
 from flask_restful import Resource
+from flask import jsonify
 import pymysql
 
 class Users(Resource):    
     def db_init(self):
-        db = pymysql.connect('localhost', 'root', 'zyhdEx-3timma-rotsiv', 'apitest')
+        db = pymysql.connect(host = 'localhost', user = 'root', password = 'zyhdEx-3timma-rotsiv', db = 'apitest')
         cursor = db.cursor(pymysql.cursors.DictCursor)
         return db, cursor
+    
+    def get(self):
+        db, cursor = self.db_init()
+        sql = 'SELECT * FROM apitest.users'
+        cursor.execute(sql)
+        db.commit()
+        users = cursor.fetchall()
+        db.close()
+        return jsonify({'data' : users})
