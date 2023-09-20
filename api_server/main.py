d506eafa347187d990d8fb26cffa4ea2b5959206
ns3pw6 <yann.u7426@gmail.com>
2023-09-20 13:38:44 +0800

    Deposit and Withdraw complete

--- api_server/main.py
+++ api_server/main.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Api
 from resources.user import Users, User
 from resources.account import Accounts, Account
+import pymysql
+import traceback
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,6 +17,60 @@ api.add_resource(Account, '/user/<user_id>/account/<id>')
 @app.route('/')
 def index():
     return 'Hello World'
+
+@app.route('/user/<user_id>/account/<id>/deposit', methods = ['POST'])
+def deposit(user_id, id):
+    db, cursor, account = get_account(id)
+    money = request.get_json()['money']
+    balance = account['balance'] + int(money)
+    sql = """
+        UPDATE apitest.account SET balance = {} WHERE id = {} AND deleted IS NOT TRUE
+    """.format(balance, id)
+    
+    response = {}
+    try:
+        cursor.execute(sql)
+        response['msg'] = 'Success'
+    except:
+        traceback.print_exc()
+        response['msg'] = 'Failed'
+    
+    db.commit()
+    db.close()
+    return jsonify(response)
+    
+    
+@app.route('/user/<user_id>/account/<id>/withdraw', methods = ['POST'])
+def withdraw(user_id, id):
+    db, cursor, account = get_account(id)
+    money = request.get_json()['money']
+    balance = account['balance'] - int(money)
+    sql = """
+        UPDATE apitest.account SET balance = {} WHERE id = {} AND deleted IS NOT TRUE
+    """.format(balance, id)
+    
+    response = {}
+    try:
+        cursor.execute(sql)
+        response['msg'] = 'Success'
+    except:
+        traceback.print_exc()
+        response['msg'] = 'Failed'
+    
+    db.commit()
+    db.close()
+    return jsonify(response)
+ 
+        
+def get_account(id):
+    db = pymysql.connect(host = 'localhost', user = 'root', password = 'zyhdEx-3timma-rotsiv', db = 'apitest')
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    sql = """
+        SELECT * FROM apitest.account WHERE id = '{}' AND deleted IS NOT TRUE
+    """.format(id)
+    
+    cursor.execute(sql)
+    return db, cursor, cursor.fetchone()
 
 if __name__ == '__main__':
     app.debug = True
